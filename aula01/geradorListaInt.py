ad06e8b97b6df1e04eaa70f4574a123373c993cf
Lucca Sindeaux <luccasindeaux@gmail.com>
2025-08-11 21:08:56 -0300

    upando aula01

---
+++ aula01/geradorListaInt.py
@@ -0,0 +1,12 @@
+'''
+Crie uma função que receba como parâmetro a quantidade de elementos e o valor maximo a ser gerado aleatoriamente
+'''
+
+import random
+
+def gerador_lista_inteiros(tamanhp_lista: int = 10, intervalo_maximo: int = 50):
+    return True
+
+
+
+# Busca Sequenacial
